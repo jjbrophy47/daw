af217bf9749e436b973d1bf906f92206f3a4c96d
Jonathan Brophy <jonathanbrophy47@gmail.com>
2022-07-12 14:13:19 -0700

    fixing primer

--- jobs/influence/primer.sh
+++ jobs/influence/primer.sh
@@ -6,14 +6,12 @@ ncpu=$5
 time=$6
 partition=$7
 
-for model in ${model_list[@]}; do
-    job_name=A_${dataset}_${model}_${method}_${k}
+job_name=A_${dataset}_${model}_${method}_${k}
 
-    sbatch --cpus_per_task=$ncpu \
-        --time=$time \
-        --partition=$partition \
-        --job-name=$job_name \
-        --output=jobs/logs/influence/$job_name \
-        --error=jobs/errors/influence/$job_name \
-        jobs/influence/runner.sh $dataset $model $method $k
-done
+sbatch --cpus_per_task=$ncpu \
+    --time=$time \
+    --partition=$partition \
+    --job-name=$job_name \
+    --output=jobs/logs/influence/$job_name \
+    --error=jobs/errors/influence/$job_name \
+    jobs/influence/runner.sh $dataset $model $method $k
